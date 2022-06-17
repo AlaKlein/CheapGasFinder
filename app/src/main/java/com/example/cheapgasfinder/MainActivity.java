d6e2da1a90a0e34471dc17070eb488ed191b1d84
AlaKlein <alaeklein@gmail.com>
2022-06-17 00:30:03 -0300

    Implementation of login and user creation saving and retrieving data from firebase DB.

--- app/src/main/java/com/example/cheapgasfinder/MainActivity.java
+++ app/src/main/java/com/example/cheapgasfinder/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.cheapgasfinder;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.math.BigInteger;
 import java.security.MessageDigest;
@@ -17,13 +25,16 @@ import java.security.NoSuchAlgorithmException;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FirebaseAuth mAuth;
+
+
     private EditText edUsername = null;
     private EditText edPassword = null;
     private Button btLogin = null;
     private Button btRegister = null;
     private TextView tvError = null;
-    String login = "ala.klein@teste.com";
-    String password = "25d55ad283aa400af464c76d713c07ad";
+    //String login = "ala.klein@teste.com";
+    //String password = "25d55ad283aa400af464c76d713c07ad";
 
     public static String getMd5Hash(String input) {
         try {
@@ -47,6 +58,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+       mAuth = FirebaseAuth.getInstance();
        edUsername = (EditText) findViewById( R.id.et_login);
        edPassword = (EditText) findViewById( R.id.et_password);
        btLogin = (Button) findViewById ( R.id.bt_login);
@@ -56,12 +68,22 @@ public class MainActivity extends AppCompatActivity {
         btLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (edUsername.getText().toString().equals(login) && (getMd5Hash(edPassword.getText().toString()).equals(password))) {
-                    Intent intent = new Intent(MainActivity.this, MapsActivity.class);
-                    intent.setAction(Intent.ACTION_VIEW);
-                    startActivity(intent);
-                }else{
-                  tvError.setText("Incorrect user or password!");
+                //if (edUsername.getText().toString().equals(login) && (getMd5Hash(edPassword.getText().toString()).equals(password))) {
+                String email = edUsername.getText().toString();
+                String password = edPassword.getText().toString();
+                if (!TextUtils.isEmpty(email) || !TextUtils.isEmpty(password)) {
+                    mAuth.signInWithEmailAndPassword(email,getMd5Hash(password)).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+                                intent.setAction(Intent.ACTION_VIEW);
+                                startActivity(intent);
+                            }else {
+                                tvError.setText("Incorrect user or password!");
+                            }
+                        }
+                    });
                 }
             }
         });
@@ -76,5 +98,14 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentuser = FirebaseAuth.getInstance().getCurrentUser();
+        if (currentuser == null) {
+
+        }
     }
 }
--- app/src/main/java/com/example/cheapgasfinder/SignUp.java
+++ app/src/main/java/com/example/cheapgasfinder/SignUp.java
@@ -1,19 +1,30 @@
 package com.example.cheapgasfinder;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 
 public class SignUp extends AppCompatActivity {
 
-    private EditText edSignUpUsername = null;
+    private FirebaseAuth mAuth;
+    private EditText edSignUpEmail = null;
     private EditText edSignUpPassword = null;
     private EditText edSignUpPassword2 = null;
     private Button btSubmit = null;
     private Button btCancel = null;
+    private TextView tvError = null;
 
 
     @Override
@@ -21,15 +32,42 @@ public class SignUp extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
-        edSignUpUsername = findViewById( R.id.edSignUpEmail);
-        edSignUpPassword = findViewById( R.id.edSignUpPassword);
-        edSignUpPassword2 = findViewById( R.id.edSignUpPassword2);
-        btSubmit = findViewById( R.id.btSubmit);
-        btCancel = findViewById( R.id.btCancel);
+        mAuth = FirebaseAuth.getInstance();
+
+        edSignUpEmail = findViewById(R.id.edSignUpEmail);
+        edSignUpPassword = findViewById(R.id.edSignUpPassword);
+        edSignUpPassword2 = findViewById(R.id.edSignUpPassword2);
+        btSubmit = findViewById(R.id.btSubmit);
+        btCancel = findViewById(R.id.btCancel);
+        tvError = findViewById(R.id.tv_error2);
+
+        btSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = edSignUpEmail.getText().toString();
+                String password = edSignUpPassword.getText().toString();
+                String password2 = edSignUpPassword2.getText().toString();
+
+                if (!TextUtils.isEmpty(email) || !TextUtils.isEmpty(password)
+                        || !TextUtils.isEmpty(password2)) {
+
+
+                    mAuth.createUserWithEmailAndPassword(email, MainActivity.getMd5Hash(password)).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                finish();
+                            }
+                        }
+                    });
+                } else {
+                    tvError.setText("ué");
+                }
+            }
+        });
 
         btCancel.setOnClickListener(new View.OnClickListener() {
             @Override
-
             public void onClick(View view) {
                 finish();
             }
