3be9e700bcb8d2a12c11fee1354aa22dba876422
AlaKlein <alaeklein@gmail.com>
2022-06-18 14:18:41 -0300

    Created Gas Station Model. Implementation of Gas Station CRUD registration and save method.

--- app/src/main/java/com/example/cheapgasfinder/Home.java
+++ app/src/main/java/com/example/cheapgasfinder/Home.java
@@ -2,13 +2,60 @@ package com.example.cheapgasfinder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Home extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private Button btRegisterGs = null;
+    private Button btMap = null;
+    private Button btLogout = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        mAuth = FirebaseAuth.getInstance();
+        btRegisterGs = findViewById(R.id.btRegisterGasStation);
+        btMap = findViewById(R.id.btMap);
+        btLogout = findViewById(R.id.btLogout);
+
+        btRegisterGs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Home.this, GasStationRegistration.class);
+                intent.setAction(Intent.ACTION_VIEW);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        btMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Home.this, MapsActivity.class);
+                intent.setAction(Intent.ACTION_VIEW);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        btLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mAuth.signOut();
+                Intent intent = new Intent(Home.this, MainActivity.class);
+                intent.setAction(Intent.ACTION_VIEW);
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }
 }
